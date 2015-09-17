aaa06b344c094100db263d8f7953917a2dbf4b75
khyngkillers <anthonylepine2@gmail.com>
2015-09-17 14:56:27 +0200

    Début de la vue

---
+++ page/connexion/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once getcwd().'/../../controler/connexion/mainConnexion.php';
+?>
--- view/gabarit.php
+++ view/gabarit.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title><?= $titre ?></title>
-</head>
+    <head>
+        <link href="/nostromo/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="/nostromo/stylesheet.css"/>
+        <title>Nostromo</title>
+    </head>
 <body>
+
+	<?= $menu ?>
 	<?= $contenu ?>
 </body>
 </html>
