12692efb830fbf328983246ed0a865b9e09c30ae
KiyozEnc <kevinturmel@gmail.com>
2015-12-07 15:40:40 +0100

    Mise à jour fichier boutique vue

--- views/boutique/v_VoirArticle.php
+++ views/boutique/v_VoirArticle.php
@@ -1,36 +1,28 @@
 <?php
 require_once("models/m_Connexion.php");
-
 ?>
 <div class="jumbotron">
 	<?php if(isset($_SESSION['valid'])) { ?>
-	<div class="alert alert-success" role="alert">
-		<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-		<?= $_SESSION['valid'] ?>
-	</div>
+		<div class="alert alert-success" role="alert">
+			<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+			<?= $_SESSION['valid'] ?>
+		</div>
 	<?php } ?>
 	<?php if(isset($_SESSION['error'])) { ?>
-	<div class="alert alert-danger" role="alert">
-		<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-		<span class="sr-only">Error:</span>
-		<?= $_SESSION['error'] ?>
-	</div>
+		<div class="alert alert-danger" role="alert">
+			<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+			<span class="sr-only">Error:</span>
+			<?= $_SESSION['error'] ?>
+		</div>
 	<?php } ?>
 	<?php echo $article->getDesignation(); ?> → Prix unitaire : <?php echo $article->getPu(); ?> €
-	<br><br><br>
-
 	<form action="?uc=monPanier&action=ajouterArticle&ref=<?php echo $article->getNumArt(); ?>" method="POST" role="form" autocomplet>
-
 		<div class="form-group">
-			<label for="">Quantitée voulu</label>
+			<label for="">Quantité voulue</label>
 			<input name="qte" type="text" class="form-control" id="" placeholder="Quantitée">
 		</div>
-
-
-
-		<button type="submit" class="btn btn-primary">Acheté</button>
+		<button type="submit" class="btn btn-primary">Ajouter au panier</button>
 	</form>
 </div>
-
 <?php if(isset($_SESSION['valid'])) { unset($_SESSION['valid']); } ?>
 <?php if(isset($_SESSION['error'])) { unset($_SESSION['error']); } ?>
--- views/boutique/v_VoirBoutique.php
+++ views/boutique/v_VoirBoutique.php
@@ -4,43 +4,41 @@ require_once("models/m_Connexion.php");
 ?>
 <div class="jumbotron">
 	<?php if(isset($_SESSION['valid'])) { ?>
-	<div class="alert alert-success" role="alert">
-		<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-		<?= $_SESSION['valid'] ?>
-	</div>
+		<div class="alert alert-success" role="alert">
+			<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+			<?= $_SESSION['valid'] ?>
+		</div>
 	<?php } ?>
 	<?php if(isset($_SESSION['error'])) { ?>
-	<div class="alert alert-danger" role="alert">
-		<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-		<span class="sr-only">Error:</span>
-		<?= $_SESSION['error'] ?>
-	</div>
+		<div class="alert alert-danger" role="alert">
+			<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+			<span class="sr-only">Error:</span>
+			<?= $_SESSION['error'] ?>
+		</div>
 	<?php } ?>
 	<table class="table table-bordered table-hover table-condensed">
 		<legend>Liste des articles disponible</legend>
 		<thead>
-			<tr>
-				<th>Numéro de l'article</th>
-				<th>Désignation</th>
-				<th>Prix unitaire</th>
-				<?php if(Connexion::sessionOuverte()) { ?><th>Actions</th><?php } ?>
-			</tr>
+		<tr>
+			<th>Numéro de l'article</th>
+			<th>Désignation</th>
+			<th>Prix unitaire</th>
+			<?php if(Connexion::sessionOuverte()) { ?><th>Actions</th><?php } ?>
+		</tr>
 		</thead>
 		<tbody>
-			<?php
-			foreach ($tabArt as $art => $unArt)
-				{ ?>
+		<?php
+		foreach ($tabArt as $art => $unArt)
+		{ ?>
 			<tr>
 				<td> <?php echo $unArt->getNumArt(); ?> </td>
 				<td> <?php echo $unArt->getDesignation(); ?> </td>
 				<td><?php echo $unArt->getPu(); ?> €</td>
 				<?php if(Connexion::sessionOuverte()) { ?><th><a href="?uc=materiel&action=voirArticle&article=<?php echo $unArt->getNumArt(); ?>" type="button" class="btn btn-default">Détails</a></th><?php } ?>
-
 			</tr>
-			<?php } ?>
+		<?php } ?>
 		</tbody>
 	</table>
 </div>
-
 <?php if(isset($_SESSION['valid'])) { unset($_SESSION['valid']); } ?>
 <?php if(isset($_SESSION['error'])) { unset($_SESSION['error']); } ?>
