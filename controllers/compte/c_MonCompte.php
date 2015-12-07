f7070aa184ac930323cb95a2c787688feaf427d7
KiyozEnc <kevinturmel@gmail.com>
2015-12-07 16:20:54 +0100

    Coucou

---
+++ controllers/compte/c_MonCompte.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_GET['action']))
+    $action = $_GET['action'];
+else
+    $action = "voirMonCompte";
+
+switch ($action)
+{
+    case 'voirMonCompte' :
+        include("views/compte/v_VoirProfile.php");
+
+        ; break;
+}
