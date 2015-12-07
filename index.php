22591ed3a3ef57c48dc49a530ac2dd11720ab2ef
KiyozEnc <kevinturmel@gmail.com>
2015-12-07 17:19:23 +0100

    Mise à jour voirArticle

--- index.php
+++ index.php
@@ -71,5 +71,6 @@ session_start(); ?>
 			header("Location:?uc=index");
 		} ?>
 	</div>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript"></script>
 </body>
 </html>
