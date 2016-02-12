09ea87ef7f39c43c360ce4bf87e5a64b6014c395
KiyozEnc <kevinturmel@gmail.com>
2016-02-12 11:39:26 +0100

    Mise à jour vues boutique coding standard Zend

--- views/boutique/v_VoirArticle.php
+++ views/boutique/v_VoirArticle.php
@@ -1,29 +1,41 @@
-<?php if(isset($_SESSION['valid'])) { ?>
+<?php
+if (array_key_exists('valid', $_SESSION)) { ?>
     <div class="alert alert-success" role="alert">
         <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-        <?= $_SESSION['valid'] ?>
+        <?php echo $_SESSION['valid'] ?>
     </div>
 <?php } ?>
-<?php if(isset($_SESSION['error'])) { ?>
+<?php
+if (array_key_exists('error', $_SESSION)) { ?>
     <div class="alert alert-danger" role="alert">
         <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
         <span class="sr-only">Error:</span>
-        <?= $_SESSION['error'] ?>
+        <?php echo $_SESSION['error'] ?>
     </div>
 <?php } ?>
-<h2><?php echo $article->getDesignation(); ?> — Référence : <?= $article->getNumArt() ?></h2>
+<h2>
+    <?php echo $article->getDesignation(); ?>
+    — Référence : <?php echo $article->getNumArt() ?>
+</h2>
 <p>Prix unitaire : <?php echo $article->getPu(); ?> € TTC</p>
-<form action="?uc=monPanier&action=ajouterArticle&ref=<?php echo $article->getNumArt(); ?>" method="POST" role="form" autocomplete="off">
+<form action="?uc=monPanier&action=ajouterArticle&ref=<?php echo $article->getNumArt(); ?>"
+      method="POST"
+      role="form"
+      autocomplete="off">
     <div class="row">
         <div class="col-sm-3">
             <div class="form-group">
                 <label for="">Quantité voulue</label>
-                <input name="qte" type="text" class="form-control" id="" placeholder="Quantité">
+                <input name="qte" type="number" class="form-control" id="" placeholder="Quantité">
             </div>
         </div>
     </div>
     <a href="?uc=materiel" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span> Retour</a>
     <button type="submit" class="btn btn-primary">Ajouter au panier</button>
 </form>
-<?php if(isset($_SESSION['valid'])) { unset($_SESSION['valid']); } ?>
-<?php if(isset($_SESSION['error'])) { unset($_SESSION['error']); } ?>
+<?php if (array_key_exists('valid', $_SESSION)) {
+    unset($_SESSION['valid']);
+} ?>
+<?php if (array_key_exists('error', $_SESSION)) {
+    unset($_SESSION['error']);
+}
--- views/boutique/v_VoirBoutique.php
+++ views/boutique/v_VoirBoutique.php
@@ -1,38 +1,54 @@
-<?php if(isset($_SESSION['valid'])) { ?>
-	<div class="alert alert-success" role="alert">
-		<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-		<?= $_SESSION['valid'] ?>
-	</div>
+<?php
+if (array_key_exists('valid', $_SESSION)) { ?>
+    <div class="alert alert-success" role="alert">
+        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+        <?php echo $_SESSION['valid'] ?>
+    </div>
 <?php } ?>
-<?php if(isset($_SESSION['error'])) { ?>
-	<div class="alert alert-danger" role="alert">
-		<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-		<span class="sr-only">Error:</span>
-		<?= $_SESSION['error'] ?>
-	</div>
+<?php
+if (array_key_exists('error', $_SESSION)) { ?>
+    <div class="alert alert-danger" role="alert">
+        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+        <span class="sr-only">Error:</span>
+        <?php echo $_SESSION['error'] ?>
+    </div>
 <?php } ?>
 <table class="table table-bordered table-hover table-condensed">
-	<h2>Articles disponibles</h2>
-	<thead>
-	<tr>
-		<th>Numéro de l'article</th>
-		<th>Désignation</th>
-		<th>Prix unitaire</th>
-		<?php if(Connexion::sessionOuverte()) { ?><th>Actions</th><?php } ?>
-	</tr>
-	</thead>
-	<tbody>
-	<?php
-	foreach ($tabArt as $art => $unArt)
-	{ ?>
-		<tr>
-			<td> <?php echo $unArt->getNumArt(); ?> </td>
-			<td> <?php echo $unArt->getDesignation(); ?> </td>
-			<td><?php echo $unArt->getPu(); ?> €</td>
-			<?php if(Connexion::sessionOuverte()) { ?><th><a href="?uc=materiel&action=voirArticle&article=<?php echo $unArt->getNumArt(); ?>" type="button" class="btn btn-default">Détails</a></th><?php } ?>
-		</tr>
-	<?php } ?>
-	</tbody>
+    <h2>Articles disponibles</h2>
+    <thead>
+    <tr>
+        <th>Numéro de l'article</th>
+        <th>Désignation</th>
+        <th>Prix unitaire</th>
+        <?php
+        if (Connexion::sessionOuverte()) { ?>
+            <th>Actions</th>
+        <?php } ?>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($tabArt as $art => $unArt) { ?>
+        <tr>
+            <td><?php echo $unArt->getNumArt(); ?></td>
+            <td><?php echo $unArt->getDesignation(); ?></td>
+            <td><?php echo $unArt->getPu(); ?> €</td>
+            <?php
+            if (Connexion::sessionOuverte()) { ?>
+                <th>
+                    <a href="?uc=materiel&action=voirArticle&article=<?php echo $unArt->getNumArt(); ?>"
+                       type="button"
+                       class="btn btn-default">Détails
+                    </a>
+                </th>
+            <?php } ?>
+        </tr>
+    <?php } ?>
+    </tbody>
 </table>
-<?php if(isset($_SESSION['valid'])) { unset($_SESSION['valid']); } ?>
-<?php if(isset($_SESSION['error'])) { unset($_SESSION['error']); } ?>
+<?php if (array_key_exists('valid', $_SESSION)) {
+    unset($_SESSION['valid']);
+} ?>
+<?php if (array_key_exists('error', $_SESSION)) {
+    unset($_SESSION['error']);
+}
