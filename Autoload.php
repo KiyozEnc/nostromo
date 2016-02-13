e415c847a65a48f8d9c5b39ace5513f6e6be67ca
KiyozEnc <kevinturmel@gmail.com>
2016-02-13 03:53:45 +0100

    Autoload fix for linux servers

--- Autoload.php
+++ Autoload.php
@@ -14,7 +14,16 @@ class Autoloader
 
     public static function autoload($class)
     {
-        $class = str_replace(__NAMESPACE__.DS, '', $class);
+        $parts = preg_split('#\\\#', $class);
+
+        $className = array_pop($parts);
+
+        $path = implode('\\', $parts);
+
+        $class = str_replace($path, strtolower($path), $class);
+        $class = str_replace('\\', DS, $class);
+        $class = str_replace(strtolower(__NAMESPACE__).DS, '', $class);
+
         require ROOT.$class.'.php';
     }
 }
